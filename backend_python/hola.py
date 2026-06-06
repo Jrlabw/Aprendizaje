035023d725c1a60a77a915ff5e135c8e63ad3936
Jr.2907 <turicopipo272@gmail.com>
2026-06-05 19:29:05 -0500

    cambios en archivos

--- backend_python/hola.py
+++ backend_python/hola.py
@@ -4,7 +4,7 @@ num2 = 5
 if num1 > num2:
     print("este es el numero mayor", num1)
 else:
-    print("este es el numero mayor", num2)  # crvg
+    print("este es el numero mayor", num2)  
     """
     este es un docstring
     """
